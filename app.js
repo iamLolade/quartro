62757570998d89046d50a0a14c497e8e4b767384
iamLolade <adeniyiotemade@gmail.com>
2022-01-09 02:53:34 +0100

    Updated view engine

--- app.js
+++ app.js
@@ -6,20 +6,23 @@ const app = express();
 //view engine
 app.set("view engine", "ejs");
 
+//middleware
+app.use(express.static("public"));
+
 //route
 app.get("/", (req, res) => {
     res.render("index", { title: "Home" })
 })
 
 app.get("/about", (req, res) => {
-    res.render("about")
+    res.render("about", { title: "About" })
 })
 
 app.get("/blogs/create", (req, res) => {
-    res.render("create")
+    res.render("create", { title: "New Blog" })
 })
 app.use((req, res) => {
-    res.status(404).render("404")
+    res.status(404).render("404", { title: "404" })
 })
 
 //listen to request
